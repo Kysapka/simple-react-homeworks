e494df906cf64563468f0860ccb3a6c3475b57d4
Артем <kysapka@gmail.com>
2021-09-21 17:24:52 +0300

    task 9 complited

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,60 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import style from './HW9.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString()
+    const stringDate = date?.toLocaleDateString()
 
     return (
-        <div>
+        <div className={style.container}>
+
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
+                <h1 className={style.neonText}>
                 {stringTime}
+                </h1>
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
+                <div className={style.dateBlock}>
+                <h2 className={style.neonText}>
+                    {show && stringDate}
+                </h2>
+                    <div>
+                        <SuperButton onClick={start}>start</SuperButton>
+                        <SuperButton onClick={stop}>stop</SuperButton>
+                    </div>
                 </div>
-            )}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+
 
         </div>
     )
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Clock from './Clock'
+import style from './HW9.module.css'
 
 function HW9() {
     return (
-        <div>
+        <div className={style.HW9}>
             <hr/>
             homeworks 9
 

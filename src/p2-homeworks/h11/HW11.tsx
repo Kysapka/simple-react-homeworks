6e62f8eaaaa23843374f459f87c3c1540c9c8866
Артем <kysapka@gmail.com>
2021-10-04 23:11:22 +0300

    task 11 hard task...

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,27 +1,35 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import SuperDoubleRange, {valueType} from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import style from './h11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeRangeHandler = ([a, b]:[number, number]) => {
+        setValue1(a)
+        setValue2(b)
+    }
+
+    // let value:valueType = [value1, value2]
+
     return (
-        <div>
+        <div className={style.h11}>
             <hr/>
             homeworks 11
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
+                <SuperRange value={value1} onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
+                <SuperDoubleRange values={[value1, value2]} onChangeRange={onChangeRangeHandler}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,68 @@
-import React from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import s from "../c7-SuperRange/SuperRange.module.css";
 
-type SuperDoubleRangePropsType = {
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+export type valueType = [number, number]
+type SuperDoubleRangePropsType = DefaultInputPropsType & {
     onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange2?: (value: [number, number]) => void
+    values?: valueType
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        type,
+        onChange, onChangeRange,
+        className,
+        values,
+        ...restProps
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    // const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    //     onChange && onChange(e) // сохраняем старую функциональность
+    //
+    //     onChangeRange && onChangeRange([+e.currentTarget.value, 0])
+    //     // onChangeRange2 && onChangeRange2([+e.currentTarget.value, 0])
+    // }
+
+    const onChangeCallback1 = (number1: number) => {
+        values && onChangeRange && onChangeRange([number1, values[1]])
+    }
+    const onChangeCallback2 = (number2: number) => {
+        values && onChangeRange && onChangeRange([values[0], number2])
+    }
+
+
+    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+
     return (
         <>
-            DoubleRange
+            <input
+                value={values && values[0]}
+                type={'range'}
+                onChange={(e)=> {
+                    onChangeCallback1(+e.currentTarget.value)
+                }}
+                className={finalRangeClassName && s.input1 }
+
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+
+            <input
+                value={values && values[1]}
+                type={'range'}
+                onChange={(e)=> {
+                    onChangeCallback2(+e.currentTarget.value)
+                }}
+                className={finalRangeClassName && s.input2 }
+
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+
         </>
     )
 }
